d0c3ef0572417930cf636b91126cbfeaaede2678
43312024 <43312024@qq.com>
2023-02-21 21:06:51 +0800

    first commit

---
+++ cards.py
@@ -0,0 +1,29 @@
+'''
+所有牌的信息
+颜色：red, yellow, blue , green, white, colorful
+数字：1,2,3,4,5
+普通颜色各数字对应张数：
+    1 ： 3张
+    2 ： 2张
+    3 ： 2张
+    4 ： 2张
+    5 ： 1张
+彩色各数字对应张数：
+    1 ： 1张
+    2 ： 1张
+    3 ： 1张
+    4 ： 1张
+    5 ： 1张
+'''
+
+
+class card():
+    def __init__(self):
+        self.unknow = True
+        self.red = True
+        self.yellow = True
+        self.
+
+
+
+
