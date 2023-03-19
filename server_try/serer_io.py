7a088e755beb672fd28cba046ab835c8c7c3370b
43312024 <43312024@qq.com>
2023-03-19 21:08:20 +0800

    尝试socketio

--- server_try/serer_io.py
+++ server_try/serer_io.py
@@ -17,12 +17,14 @@ def my_message(sid, data):
     print('message ', data)
     for pid in room:
         socketio.Server.send(sio, f"房间有{len(room)}个人", skip_sid=pid)
-        sio.call("my_message", f"房间有{len(room)}个人", sid=pid)
+        sio.call("my_message", f"有人加入了，房间有{len(room)}个人", sid=pid)
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
     room.remove(sid)
+    for pid in room:
+        sio.call("my_message", f"有人离开了，房间有{len(room)}个人", sid=pid)
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
